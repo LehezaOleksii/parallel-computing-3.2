3cdd03e13e0603204c3858d3735945f37bdf5a6e
LehezaOleksii <oleksii.leheza@gmail.com>
2024-04-29 16:04:06 +0300

    update 2 lab

--- lab2_tpo/lab2_tpo/src/main/java/oleksii/leheza/labs/tpo/Main.java
+++ lab2_tpo/lab2_tpo/src/main/java/oleksii/leheza/labs/tpo/Main.java
@@ -3,14 +3,14 @@ package oleksii.leheza.labs.tpo;
 import oleksii.leheza.labs.tpo.basic.BasicMatrixMultiply;
 import oleksii.leheza.labs.tpo.fox.FoxMatrixMultiplication;
 import oleksii.leheza.labs.tpo.matrix.Matrix;
-import oleksii.leheza.labs.tpo.striped.alghorihm.StripedMatrixMultiplication;
-import oleksii.leheza.labs.tpo.util.MatrixUtil;
 import oleksii.leheza.labs.tpo.matrix.Result;
+import oleksii.leheza.labs.tpo.util.MatrixUtil;
 
 public class Main {
 
     public static void main(String[] args) {
         int matrixSize = 1000;
+        int threadAmount = 10;
         MatrixUtil matrixUtil = new MatrixUtil();
         Matrix matrix = matrixUtil.initializeRandomMatrix(matrixSize);
 //       printMatrix(matrix);
@@ -21,33 +21,16 @@ public class Main {
 
         BasicMatrixMultiply basicMatrixMultiply = new BasicMatrixMultiply();
         FoxMatrixMultiplication foxMatrixMultiplication = new FoxMatrixMultiplication();
-        StripedMatrixMultiplication stripedMatrixMultiplication = new StripedMatrixMultiplication(matrix, matrix, threadAmount, result2);
         long startTime1 = System.currentTimeMillis();
-        basicMatrixMultiply.multiplyMatrix(matrix, matrix,result1);
+        basicMatrixMultiply.multiplyMatrix(matrix, matrix, result1);
         long endTime1 = System.currentTimeMillis();
-
-        long startTime2 = System.currentTimeMillis();
-        stripedMatrixMultiplication.multiply();
-        long endTime2 = System.currentTimeMillis();
-
         long startTime3 = System.currentTimeMillis();
-        foxMatrixMultiplication.foxMatrixMultiply(matrix, matrix, result3,threadAmount);
+        foxMatrixMultiplication.foxMatrixMultiply(matrix, matrix, result3, threadAmount);
         long endTime3 = System.currentTimeMillis();
         double resultTime1 = endTime1 - startTime1;
-        double resultTime2 = endTime2 - startTime2;
         double resultTime3 = endTime3 - startTime3;
-
-
-//        System.out.println("---------------Result Matrix 1---------------");
-//        printMatrix(result1.getResultMatrix());
-//        System.out.println("---------------Result Matrix 2---------------");
-//        printMatrix(result2.getResultMatrix());
-//        System.out.println("---------------Result Matrix 3---------------");
-//        printMatrix(result3.getResultMatrix());
-
         System.out.println("---------------Result---------------" +
                 "\nSimple matrix multiplication: " + resultTime1 +
-                "\nStriped matrix multiplication: " + resultTime2 + "; speedup = " + resultTime1 / resultTime2+
                 "\nFox matrix multiplication: " + resultTime3 + "; speedup = " + resultTime1 / resultTime3
         );
     }
--- lab2_tpo/lab2_tpo/src/main/java/oleksii/leheza/labs/tpo/striped/alghorihm/StripedMatrixMultiplication.java
+++ lab2_tpo/lab2_tpo/src/main/java/oleksii/leheza/labs/tpo/striped/alghorihm/StripedMatrixMultiplication.java
@@ -1,125 +1,125 @@
-package oleksii.leheza.labs.tpo.striped.alghorihm;
-
-import oleksii.leheza.labs.tpo.matrix.Result;
-import oleksii.leheza.labs.tpo.matrix.Matrix;
-import oleksii.leheza.labs.tpo.striped.Synchronizer;
-
-import java.util.LinkedList;
-import java.util.List;
-
-public class StripedMatrixMultiplication {
-
-    private Matrix firstMatrix;
-    private Matrix secondMatrix;
-    private int threadsAmount;
-    private Result result;
-    private final Object lock = new Object();
-
-    private Synchronizer synchronizer;
-
-
-    public StripedMatrixMultiplication(Matrix firstMatrix, Matrix secondMatrix, int threadsAmount, Result result) {
-        this.firstMatrix = firstMatrix;
-        this.secondMatrix = secondMatrix;
-        this.threadsAmount = threadsAmount;
-        this.result = result;
-    }
-
-    public void multiply() {
-        if (firstMatrix.getRow(0).length != secondMatrix.getColumn(0).length) {
-            throw new RuntimeException("Different length");
-        }
-
-        //prepare thread
-        synchronizer = new Synchronizer();
-        HeadThread headThread = new HeadThread(result, 0, firstMatrix.getMatrixSize(), lock, synchronizer);
-        List<ClassicThread> threads = new LinkedList<>();
-        threads.add(headThread);
-        ClassicThread lastClassicThread = null;
-        //Create matrix threads
-        int iteration = 1;
-        for (; iteration < threadsAmount - 1; iteration++) {
-            if (lastClassicThread == null) {
-                lastClassicThread = new ClassicThread(result, iteration, firstMatrix.getMatrixSize(), synchronizer);
-                headThread.setSubThread(lastClassicThread);
-            } else {
-                ClassicThread classicThread = new ClassicThread(result, iteration, firstMatrix.getMatrixSize(), synchronizer);
-                lastClassicThread.setSubThread(classicThread);
-                lastClassicThread = classicThread;
-            }
-            threads.add(lastClassicThread);
-        }
-        EndThread endThread = new EndThread(result, threadsAmount - 1, firstMatrix.getMatrixSize(), synchronizer);
-        lastClassicThread.setSubThread(endThread);
-        threads.add(endThread);
-        //start threads
-        for (Thread process : threads) {
-            process.start();
-        }
-        //maintain threads
-        for (int i = 0; i < firstMatrix.getMatrix().length; ) {
-            for (int n = 0; n < firstMatrix.getMatrixSize(); ) {
-                synchronized (lock) {
-                    while (!headThread.getIsNeedNewData()) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-                int addColumnAndRowAmount = 5;
-                if (headThread.getLastRowIndex() + addColumnAndRowAmount < firstMatrix.getMatrixSize() - 1) {
-                    for (int k = 0; k < addColumnAndRowAmount; k++) {
-                        setValueAndIncrementIndexes(headThread);
-                        n++;
-                    }
-                } else {
-                    setValueAndIncrementIndexes(headThread);
-                    n++;
-                }
-                headThread.setIsNeedNewData(false);
-                synchronized (lock) {
-                    lock.notifyAll();
-                }
-            }
-
-            synchronized (synchronizer) {
-                while (!synchronizer.getCycleEnd()) {
-                    try {
-                        synchronizer.wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-            i += threadsAmount;
-            iteration = i;
-            for (ClassicThread thread : threads) {
-                thread.setIteration(iteration++);
-            }
-            headThread.lastNeedRowIndex.set(0);
-            headThread.lastNeedColumnIndex.set(i);
-            synchronizer.setCycleEnd(false);
-            synchronized (synchronizer) {
-                synchronizer.notifyAll();
-            }
-        }
-        while (!synchronizer.getAlgorithmEnd()) {
-            synchronized (synchronizer) {
-                try {
-                    synchronizer.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
-
-    private void setValueAndIncrementIndexes(HeadThread headThread) {
-        headThread.addRowToQueue(firstMatrix.getRow(headThread.lastNeedRowIndex.get()));
-        headThread.addColumnToQueue(secondMatrix.getColumn(headThread.lastNeedColumnIndex.get()));
-        headThread.incrementLastColumnIndex();
-        headThread.incrementLastRowIndex();
-    }
-}
+//package oleksii.leheza.labs.tpo.striped.alghorihm;
+//
+//import oleksii.leheza.labs.tpo.matrix.Result;
+//import oleksii.leheza.labs.tpo.matrix.Matrix;
+//import oleksii.leheza.labs.tpo.striped.Synchronizer;
+//
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//public class StripedMatrixMultiplication {
+//
+//    private Matrix firstMatrix;
+//    private Matrix secondMatrix;
+//    private int threadsAmount;
+//    private Result result;
+//    private final Object lock = new Object();
+//
+//    private Synchronizer synchronizer;
+//
+//
+//    public StripedMatrixMultiplication(Matrix firstMatrix, Matrix secondMatrix, int threadsAmount, Result result) {
+//        this.firstMatrix = firstMatrix;
+//        this.secondMatrix = secondMatrix;
+//        this.threadsAmount = threadsAmount;
+//        this.result = result;
+//    }
+//
+//    public void multiply() {
+//        if (firstMatrix.getRow(0).length != secondMatrix.getColumn(0).length) {
+//            throw new RuntimeException("Different length");
+//        }
+//
+//        //prepare thread
+//        synchronizer = new Synchronizer();
+//        HeadThread headThread = new HeadThread(result, 0, firstMatrix.getMatrixSize(), lock, synchronizer);
+//        List<ClassicThread> threads = new LinkedList<>();
+//        threads.add(headThread);
+//        ClassicThread lastClassicThread = null;
+//        //Create matrix threads
+//        int iteration = 1;
+//        for (; iteration < threadsAmount - 1; iteration++) {
+//            if (lastClassicThread == null) {
+//                lastClassicThread = new ClassicThread(result, iteration, firstMatrix.getMatrixSize(), synchronizer);
+//                headThread.setSubThread(lastClassicThread);
+//            } else {
+//                ClassicThread classicThread = new ClassicThread(result, iteration, firstMatrix.getMatrixSize(), synchronizer);
+//                lastClassicThread.setSubThread(classicThread);
+//                lastClassicThread = classicThread;
+//            }
+//            threads.add(lastClassicThread);
+//        }
+//        EndThread endThread = new EndThread(result, threadsAmount - 1, firstMatrix.getMatrixSize(), synchronizer);
+//        lastClassicThread.setSubThread(endThread);
+//        threads.add(endThread);
+//        //start threads
+//        for (Thread process : threads) {
+//            process.start();
+//        }
+//        //maintain threads
+//        for (int i = 0; i < firstMatrix.getMatrix().length; ) {
+//            for (int n = 0; n < firstMatrix.getMatrixSize(); ) {
+//                synchronized (lock) {
+//                    while (!headThread.getIsNeedNewData()) {
+//                        try {
+//                            lock.wait();
+//                        } catch (InterruptedException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//                }
+//                int addColumnAndRowAmount = 5;
+//                if (headThread.getLastRowIndex() + addColumnAndRowAmount < firstMatrix.getMatrixSize() - 1) {
+//                    for (int k = 0; k < addColumnAndRowAmount; k++) {
+//                        setValueAndIncrementIndexes(headThread);
+//                        n++;
+//                    }
+//                } else {
+//                    setValueAndIncrementIndexes(headThread);
+//                    n++;
+//                }
+//                headThread.setIsNeedNewData(false);
+//                synchronized (lock) {
+//                    lock.notifyAll();
+//                }
+//            }
+//
+//            synchronized (synchronizer) {
+//                while (!synchronizer.getCycleEnd()) {
+//                    try {
+//                        synchronizer.wait();
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            }
+//            i += threadsAmount;
+//            iteration = i;
+//            for (ClassicThread thread : threads) {
+//                thread.setIteration(iteration++);
+//            }
+//            headThread.lastNeedRowIndex.set(0);
+//            headThread.lastNeedColumnIndex.set(i);
+//            synchronizer.setCycleEnd(false);
+//            synchronized (synchronizer) {
+//                synchronizer.notifyAll();
+//            }
+//        }
+//        while (!synchronizer.getAlgorithmEnd()) {
+//            synchronized (synchronizer) {
+//                try {
+//                    synchronizer.wait();
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }
+//    }
+//
+//    private void setValueAndIncrementIndexes(HeadThread headThread) {
+//        headThread.addRowToQueue(firstMatrix.getRow(headThread.lastNeedRowIndex.get()));
+//        headThread.addColumnToQueue(secondMatrix.getColumn(headThread.lastNeedColumnIndex.get()));
+//        headThread.incrementLastColumnIndex();
+//        headThread.incrementLastRowIndex();
+//    }
+//}
